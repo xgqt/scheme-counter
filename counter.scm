856f16e5164e202a8df08c235d461343c514dfe8
Maciej Barć <xgqt@riseup.net>
2021-06-29 16:03:31 +0200

    counter.scm: restructure; move loop up; ad `turn'
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

--- counter.scm
+++ counter.scm
@@ -2,8 +2,16 @@
   (define val              init-val)
   (define growth-procedure init-growth-procedure)
   (define interval         init-interval)
+  (define (loop times procedure)
+    ;; execute PROCEDURE a specified amount of TIMES
+    (cond ((> times 0)
+           (procedure)
+           (loop (- times 1) procedure)
+           ))
+    )
   (lambda (method)
     (define (internal-get var)
+      ;; return the value of VAR
       (case var
         ((val)              val)
         ((growth-procedure) growth-procedure)
@@ -12,6 +20,7 @@
         )
       )
     (define (internal-set var new)
+      ;; set value of VAR to NEW value
       (case var
         ((val)              (set! val              new))
         ((growth-procedure) (set! growth-procedure new))
@@ -19,33 +28,28 @@
         (else               #f)
         )
       )
-    (define (internal-grow runs)
-      (letrec
-          ((loop
-            (lambda (times procedure)
-              (cond ((> times 0)
-                     (procedure)
-                     (loop (- times 1) procedure)
-                     ))
-              )
-            ))
-        (loop runs
-              (lambda ()
-                (internal-set 'val (growth-procedure val interval)))
-              )
-        )
+    (define (internal-turn)
+      ;; set VAL to result of `growth-procedure' taking VAL and INTERVAL
+      (internal-set 'val (growth-procedure val interval))
       )
-    (define (internal-run)
-      (internal-grow 1)
-      val
+    (define (internal-grow runs)
+      ;; execute `internal-turn' RUNS times
+      (loop runs (lambda () (internal-turn)))
       )
     (define (internal-runs runs)
+      ;; execute `internal-grow' RUNS times and return VAL
       (internal-grow runs)
+      val
+      )
+    (define (internal-run)
+      ;; execute `internal-turn' once and return VAL
+      (internal-turn)
       val
       )
     (case method
       ((get)  internal-get)
       ((set)  internal-set)
+      ((turn) internal-turn)
       ((grow) internal-grow)
       ((runs) internal-runs)
       (else   internal-run)
