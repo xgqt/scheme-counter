61be8583e75ec1e44ccd577651d764636c51a123
Maciej Barć <xgqt@riseup.net>
2021-08-06 17:12:04 +0200

    info.rkt: update metadata
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

--- info.rkt
+++ info.rkt
@@ -4,34 +4,29 @@
 #lang info
 
 
-(define collection "counter")
+(define pkg-authors '(xgqt))
+(define pkg-desc
+  "Small counter library to use for console output, logging, etc.")
+(define version "0.0.0")
 
+(define collection "counter")
 (define scheme-source   '("counter.scm" "main.scm"))
 (define scribble-source "scribblings/counter.scrbl")
-
 (define compile-omit-paths
-  scheme-source
-  )
+  scheme-source)
 (define test-omit-paths
-  (append scheme-source `(,scribble-source))
-  )
-
+  (append scheme-source `(,scribble-source)))
 (define scribblings
-  `((
-     ,scribble-source
-     () (library) "counter"
-     ))
-  )
+  `((,scribble-source
+     () (library) "counter")))
 
 (define deps
   '(
     "base"
-    )
-  )
+    ))
 (define build-deps
   '(
     "racket-doc"
     "rackunit-lib"
     "scribble-lib"
-    )
-  )
+    ))
