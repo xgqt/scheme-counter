fa42e8f3265d1f2a346f088fbb2553d16bbd092a
Maciej Barć <xgqt@gentoo.org>
2022-07-27 01:59:42 +0200

    info.rkt: add license; format
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- info.rkt
+++ info.rkt
@@ -5,23 +5,33 @@
 
 
 (define pkg-authors '(xgqt))
+
 (define pkg-desc
   "Small counter library to use for console output, logging, etc.")
+
 (define version "1.0.0")
 
+(define license 'CC0-1.0)
+
 (define collection "counter")
+
 (define scheme-source   '("counter.scm" "main.scm"))
+
 (define scribble-source "scribblings/counter.scrbl")
+
 (define compile-omit-paths
   scheme-source)
+
 (define test-omit-paths
   (append scheme-source `(,scribble-source)))
+
 (define scribblings
   `((,scribble-source
      () (library) "counter")))
 
 (define deps
   '("base"))
+
 (define build-deps
   '("racket-doc"
     "rackunit-lib"
